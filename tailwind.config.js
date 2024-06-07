4b0154c9d0098fada3f2f27fc9a8eb1fe34db2c2
Krishna180104 <151388967+Krishna180104@users.noreply.github.com>
2024-06-07 10:17:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary":"#0A192F",
+        "secondary":"#F97316",
+        "tertiary": "#54D88",
+      },
+    },
+    screens: {
+
+
+      'lg': {'max': '2023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
+  },
+  plugins: [],
+}
