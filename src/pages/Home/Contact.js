8349a9dc3be8b21f3f423783cecef5de960212d8
Krishna Jadhav <kj317246@gmail.com>
2024-06-26 18:55:03 +0530

    first commit

--- src/pages/Home/Contact.js
+++ src/pages/Home/Contact.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import SectionTitle from '../../components/SectionTitle'
+
 function Contact() {
+  const user = {
+    name:"Krishna Jadhav",
+    email:"kj317246@gmail.com",
+    age:"20",
+    gender:"Male",
+    mobile:"8623071778",
+    country:"INDIA"
+  }
   return (
     <div>
-        <SectionTitle title="Contact Info"/>
-        <div className='flex w-full items-center sm:flex-col'>
-            {/* <div className='h-[70vh] w-1/2 sm:w-full'>
-            <dotlottie-player src="https://lottie.host/f57ad195-1ff5-457e-bcb0-a80596964b6b/3ef1dJNxOb.json" background="transparent" speed="1" autoplay></dotlottie-player>
-            </div> */}
-            <div className='flex flex-col gap-5 w-1/2 sm:w-full'>
-                <p className='text-white'>Name: Krishna Jadhav</p>
-                <p className='text-white'>Email: kj317246@gmail.com</p>
-                <p className='text-white'>Contact No: 8623071778</p>
-                <p className='text-white'>Linkedin: https://www.linkedin.com/in/krishna-jadhav-34312a257/</p>
-            </div>
-
+      <SectionTitle title = "Say Hello"/>
+      <div className="flex sm:flex-col items-center justify-between">
+        <div className="flex flex-col gap-1 ">
+        <h1 className='text-emerald-600 text-md'>{"{"}</h1>
+        {
+          Object.keys(user).map((key)=>(
+            <h1 className='ml-5 text-md'>
+              <span className='text-emerald-600'>{key}</span> <span className='text-emerald-600'>:</span> <span className='text-emerald-600'>{user[key]}</span>
+            </h1>
+          ))
+        }
+        <h1 className='text-emerald-600 text-md'>{"}"}</h1>
         </div>
+
+        <div className='h-[400px]'>
+        <dotlottie-player src="https://lottie.host/b90c06a9-073b-4533-9b58-bac7676423b8/rATJV0B70A.json" background="transparent" speed="1" autoplay></dotlottie-player>
+        </div>
+      </div>
     </div>
   )
 }
